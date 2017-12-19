37f5038ce7b0c4d5112dc4cbe4fc07d1b2206ea9
Dima <d.gachok@gmail.com>
2017-12-20 00:34:45 +0200

    fix tabs for profile page

--- src/App/Content/Profile/component.js
+++ src/App/Content/Profile/component.js
@@ -5,6 +5,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import EventIcon from 'material-ui/svg-icons/action/event';
 import PersonIcon from 'material-ui/svg-icons/social/person';
 import RateReviewIcon from 'material-ui/svg-icons/maps/rate-review';
+import ScheduleIcon from 'material-ui/svg-icons/action/schedule';
 
 class Profile extends Component {
     render() {
@@ -14,15 +15,20 @@ class Profile extends Component {
                     <Avatar className="profile__image-avatar" icon={
                         <PersonIcon  />
                     }/>
+                    <div className="text-right">
+                        <strong>Name Surname</strong>
+                        <div className="profile__subtitle">subtitle</div>
+                    </div>
                 </div>
                 <div className="profile__tabs-container">
                     <Tabs className="profile__tabs">
                         <Tab icon={<PersonIcon />}>
-
+                            Person information
                         </Tab>
                         <Tab icon={<EventIcon />}>
 
                         </Tab>
+                        <Tab icon={<ScheduleIcon />} />
                         <Tab icon={<RateReviewIcon />} />
                     </Tabs>
                 </div>
